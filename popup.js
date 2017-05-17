7adb56e17ae46e9bced104bb0c67a6b52e3c7147
Tom <auu@me.com>
2017-05-17 10:52:04 +0900

    delete realtime-translate add translation button enable all google translation supported languages

--- popup.js
+++ popup.js
@@ -1,6 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 getUserPrefs();
 
@@ -13,26 +10,122 @@ function click(e) {
 }
 
 function getUserPrefs() {
-    chrome.storage.sync.get('apikey', function (obj) {
-        console.log(obj.apikey);
-        document.getElementById('apikey').value = obj.apikey;
-    });
+  chrome.storage.sync.get(['apikey','source_lang','target_lang', 'support_lang'], function (obj) {
+
+    if (obj.apikey == undefined || obj.apikey == "") {
+      return;
+    }
+    document.getElementById('apikey').value = obj.apikey;
+    source_lang = obj.source_lang;
+    target_lang = obj.target_lang;
+
+    if (source_lang) {
+      document.getElementById('source').style = "visibility: block;";
+    }
+    if (target_lang) {
+      document.getElementById('target').style = "visibility: block;";
+    }
+
+    support_lang = obj.support_lang;
+    if (obj.apikey != undefined && obj.apikey != "" && obj.support_lang == undefined) {
+      retrieveSupportLang(obj.apikey);
+    }
+
+    if (support_lang) {
+      document.getElementById('support_lang').value = support_lang;
+      composeLangs(source_lang, target_lang);
+    }
+  });
+}
+
+function retrieveSupportLang(key) {
+  url = "https://translation.googleapis.com/language/translate/v2/languages?target=" + navigator.language
+  langs = httpGet(url + "&key=" + key);
+  chrome.storage.sync.set({'support_lang': langs.replace(/\s+/g, "")}, function() {});
+  document.getElementById('support_lang').value = langs.replace(/\s+/g, "");
 }
 
 function saveKey(e) {
   key = document.getElementById('apikey').value;
-  console.log(key);
-  chrome.storage.sync.set({'apikey': key}, function() {
-          // Notify that we saved.
+  source_lang = document.getElementById('source_lang') ? document.getElementById('source_lang').value : "ja";
+  target_lang = document.getElementById('target_lang') ? document.getElementById('target_lang').value : "en";
+  chrome.storage.sync.set({'apikey': key, 'source_lang': source_lang, 'target_lang': target_lang}, function() {
         });
-  window.close();
+
+  if (document.getElementById('support_lang').value == "") {
+    retrieveSupportLang(key);
+    composeLangs(undefined, undefined);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', saveKey);
-  }
+  document.getElementById("saveButton").addEventListener('click', saveKey);
+  document.getElementById("resetButton").addEventListener('click', resetAllStorage); 
 });
 
+function composeLangs(s,t){
+  lang_str = document.getElementById('support_lang').value
+  lang_json = JSON.parse(lang_str).data.languages;
+
+  var select_source = document.createElement("select");
+  select_source.setAttribute("id", "source_lang");
+  for (var i=0; i < lang_json.length; i++) {
+    var option = document.createElement("option");
+    option.id = lang_json[i].language;
+    option.value = lang_json[i].language;
+    option.innerHTML = lang_json[i].name;
+    if (option.id == "ja" && s == undefined) {
+      option.selected = true;
+    }
+    if (option.id == s) {
+      option.selected = true;
+    }
+    select_source.appendChild(option);
+  }
+
+  var select_target = document.createElement("select");
+  select_target.setAttribute("id", "target_lang");
+  for (var i=0; i < lang_json.length; i++) {
+    var option = document.createElement("option");
+    option.id = lang_json[i].language;
+    option.value = lang_json[i].language;
+    option.innerHTML = lang_json[i].name;
+    if (option.id == "en" && t == undefined) {
+      option.selected = true;
+    }
+    if (option.id == t) {
+      option.selected = true;
+    }
+    select_target.appendChild(option);
+  }
+  
+  document.getElementById("source").appendChild(select_source);
+  document.getElementById("target").appendChild(select_target);
+
+  document.getElementById("source").style = "visibility: block;";
+  document.getElementById("target").style = "visibility: block;";
+}
+
+function resetAllStorage() {
+  chrome.storage.sync.remove('apikey', function() {});
+  chrome.storage.sync.remove('source_lang', function() {});
+  chrome.storage.sync.remove('target_lang', function() {});
+  chrome.storage.sync.remove('support_lang', function() {});
+}
+
+function httpGet(url) {
+  var xhr = new XMLHttpRequest();
+  try {
+    xhr.open( "GET", url, false);
+    xhr.send();
+  } catch(err) {
+    console.log(err);
+    return;
+  }
+  if (xhr.status === 400) {
+    document.getElementById('key').style = "color: red";
+  }
+  return xhr.responseText;
+
+}
 
--- translate.js
+++ translate.js
@@ -1,12 +1,42 @@
 (function() {
   var TRANSLATE_KEY = undefined;
-  chrome.storage.sync.get(['apikey'], function(items) {
-    console.log('Settings retrieved', items.apikey);
+  chrome.storage.sync.get(['apikey','source_lang','target_lang'], function(items) {
+    console.debug('Settings retrieved', items);
     TRANSLATE_KEY = items.apikey;
-    init();
+
+    TARGET_LANG = items.target_lang
+    if (TARGET_LANG == undefined) {
+      TARGET_LANG = 'en';
+    }
+
+    SOURCE_LANG = items.source_lang
+    if (SOURCE_LANG == undefined) {
+      SOURCE_LANG = 'ja';
+    }
+    initExtTranslate();
   });
 
-  function init() {
+  // Observe DOM manupilation and redraw translate objects
+  var mo = new MutationObserver(function(records){
+    records.forEach(function(record){
+      if(record.type==="childList"){
+        for(var i=0;i<record.addedNodes.length;i++){
+          try {
+            if(record.addedNodes[i].className.toLowerCase()==="drawer"){
+              initExtTranslate();
+            }
+          } catch(err) {
+          }
+        }
+      }
+    });
+  });
+  mo.observe(document,{
+    childList:true,
+    subtree:true
+  });
+
+  function initExtTranslate() {
     if (TRANSLATE_KEY == undefined) {
       console.log("Google Translate API Key is not provided.");
       return;
@@ -18,49 +48,89 @@
       console.log("Cannot detect Mastodon input area.");
       return;
     }
+
+    var translate_button = document.createElement('div');
+    translate_button.setAttribute("id", "ext_google_translate_button");
+    translate_button.setAttribute("class", "text-icon-button");
+    translate_button.setAttribute("aria-expanded", "false");
+    translate_button.innerText = SOURCE_LANG.toUpperCase() + "->" + TARGET_LANG.toUpperCase();
+    translate_button.onclick = function() {
+      requestTranslate(source_area.value);
+    };
+
+    var buttons = document.getElementsByClassName('compose-form__buttons')[0];
+    buttons.appendChild(translate_button);
+
     var translate_area = document.createElement('div');
-    translate_area.setAttribute("id", "google_translate_element");
-    translate_area.innerText = "Translated result will display below";
+    translate_area.setAttribute("id", "ext_google_translate_area");
+    translate_area.innerText = "Translation result will be displayed here";
+    translate_area.setAttribute("style", "margin: 20px");
 
     var compose_form = document.getElementsByClassName('drawer__inner')[0];
     compose_form.appendChild(translate_area);
 
-    source_area.onkeyup = function() {
-      console.log(source_area.value);
-      source_text = source_area.value;
+    var enable_realtime_translate = false;
 
-      // Do not send translate request if text is too short
-      if (source_text.length < 10) {
-        return;
-      }
-      if (last_req_time == 0) {
-        requestTranslate(source_text);
-        last_req_time = 10;
-        return;
-      } else {
-        last_req_time = last_req_time - 1;
-        return;
-      }
-    };
+    if (enable_realtime_translate) {
+      source_area.onkeyup = function() {
+        console.log(source_area.value);
+        source_text = source_area.value;
+
+        // Do not send translate request if text is too short
+        if (source_text.length < 10) {
+          return;
+        }
+        if (last_req_time == 0) {
+          requestTranslate(source_text);
+          last_req_time = 10;
+          return;
+        } else {
+          last_req_time = last_req_time - 1;
+          return;
+        }
+      };
+    }
   }
 
   function requestTranslate(query) {
-    var url = "https://www.googleapis.com/language/translate/v2?target=en&key=" + TRANSLATE_KEY + "&q=" + encodeURIComponent(query);
+
+    if (query == undefined || query.length < 1) {
+      console.log("query is not specified");
+      return;
+    }
+
+    if (TRANSLATE_KEY == undefined || TRANSLATE_KEY == "") {
+      alert("Missing Google Translate API Key.");
+      return;
+    }
+
+    var q = replaceTag(query);
+
+    var url = "https://www.googleapis.com/language/translate/v2?format=html&target=" + TARGET_LANG + "&key=" + TRANSLATE_KEY + "&q=" + encodeURIComponent(q);
     var ret = httpGet(url);
 
     console.log("result is", ret);
-    console.log(ret);
     var json = JSON.parse(ret);
-    var draw_area = document.getElementById('google_translate_element');
-    draw_area.innerText = json.data.translations[0].translatedText;
+    var draw_area = document.getElementById('ext_google_translate_area');
+
+    var raw = json.data.translations[0].translatedText;
+
+    var translated = "<pre style=\"white-space: pre-wrap;\">" + replaceTag(raw) + "</pre>";
+
+    draw_area.innerHTML = translated;
   }
 
   function httpGet(url) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    console.log("translate requested to:", url);
+    xmlHttp.open( "GET", url, false);
+    xmlHttp.send(null);
+    console.log("translate requested to:", escape(url));
     return xmlHttp.responseText;
   }
+
+  function replaceTag(str){
+    return str.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  }
+
 })();
 
